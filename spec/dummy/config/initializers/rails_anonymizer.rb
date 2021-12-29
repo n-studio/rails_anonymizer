81918f77af39bc494b39ad888119ca3c5d1b156d
Matthew Nguyen <contact@n-studio.fr>
2021-12-29 16:45:38 +0800

    chore: update config files for Rails 7

--- spec/dummy/config/initializers/rails_anonymizer.rb
+++ spec/dummy/config/initializers/rails_anonymizer.rb
@@ -1,18 +1,22 @@
-begin
-  ANONYMOUS_PASSWORD = User.new(password: "password").encrypted_password
-rescue ActiveRecord::NoDatabaseError, ActiveRecord::StatementInvalid => _e
-end
+Rails.application.config.to_prepare do
+  module RailsAnonymizerConfig
+    begin
+      ANONYMOUS_PASSWORD = User.new(password: "password").encrypted_password
+    rescue ActiveRecord::NoDatabaseError, ActiveRecord::StatementInvalid => _e
+    end
+  end
 
-RailsAnonymizer.setup do |config|
-  config.black_list = {
-    "email" => -> { Faker::Internet.unique.email },
-    "encrypted_password" => -> { ANONYMOUS_PASSWORD },
-    "name" => ->(record) { "user_#{record.id}" },
-    "Admin" => {
-      "address" => -> { Faker::Address.full_address },
-    },
-    "User" => {
-      "phone_number" => -> { "+33333333333" },
-    },
-  }
+  RailsAnonymizer.setup do |config|
+    config.black_list = {
+      "email" => -> { Faker::Internet.unique.email },
+      "encrypted_password" => -> { RailsAnonymizerConfig::ANONYMOUS_PASSWORD },
+      "name" => ->(record) { "user_#{record.id}" },
+      "Admin" => {
+        "address" => -> { Faker::Address.full_address },
+      },
+      "User" => {
+        "phone_number" => -> { "+33333333333" },
+      },
+    }
+  end
 end
